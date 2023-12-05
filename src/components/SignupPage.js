acc06e0d98c52957ef7bd96b56841654b01f7c25
Jonathan Raposo <jonathan.raposo.luna@gmail.com>
2023-12-04 23:29:26 -0500

    modify signup page

--- src/components/SignupPage.js
+++ src/components/SignupPage.js
@@ -5,7 +5,20 @@ const SignupPage = () => {
     const [password, setPassword] = useState('');
     const [nationality, setNationality] = useState('');
 
-    console.log(nationality)
+
+
+    const isValidEmail = (email) => {
+        return /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/.test(email);
+
+    }
+
+    const isStrongPassword = (password) => {
+        return /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/.test(password);
+
+    }
+    console.log('is email valid: ', isValidEmail(email))
+    console.log('is password strong:', isStrongPassword(password))
+
     return (
         <div className='SignupPage'>
             <form>
@@ -13,27 +26,63 @@ const SignupPage = () => {
                 <input
                     type='email'
                     name="email"
+                    id="email"
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
+                    style={{
+                        border: email ? isValidEmail(email) ?
+                            '2px solid green'
+                            : '2px solid red'
+                            : '2px solid #ccc'
+                    }}
+
                 />
+                {isValidEmail(email) &&
+                    <p id="green-para">You typed a valid email</p>
+                }
                 <label htmlFor='password'>Password</label>
                 <input
                     type='password'
                     name="password"
+                    id='password'
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
+                    style={{
+                        border: password ? isStrongPassword(password)
+                            ? '2px solid green'
+                            : '2px solid red'
+                            : '2px solid #ccc'
+                    }}
                 />
+
+                {!isStrongPassword(password) && password && (
+                    <p id="red-para">Your password is too weak</p>
+                )}
+
                 <label htmlFor='nationality'>Nationality</label>
                 <select onChange={(e) => setNationality(e.target.value)}>
-                    <option checked>English</option>
-                    <option>German</option>
-                    <option>French</option>
+                    <option value="en">English</option>
+                    <option value="de">German</option>
+                    <option value="fr">French</option>
                 </select>
 
-                <buttom type="submit" id="signup-btn">Signup</buttom>
+                <button type="submit">Signup</button>
 
 
             </form>
+
+            <div class="display-messages">
+                <p>
+                    {
+                        nationality === 'en' ? 'Hello' :
+                            nationality === 'de' ? 'Hallo' :
+                                nationality === 'fr' ? 'Bonjour' :
+                                    ''
+                    }
+                </p>
+                {isValidEmail(email) && email && <p>Your email is: {email}</p>}
+                {isValidEmail(email) && email && <p>Your email is correct</p>}
+            </div>
         </div>
     );
 }
